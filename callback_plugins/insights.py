d670d942ae01821961db29d7c5988dd9f134fd96
Michael Dumont <mdumont@ondeck.com>
2015-06-30 17:49:48 -0400

    remove comments

--- callback_plugins/insights.py
+++ callback_plugins/insights.py
@@ -1,19 +1,5 @@
 # (C) 2012-2014, Michael Dumont <michaeldumont@gmail.com>
 
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 import requests
 import json
 import logging
@@ -27,8 +13,7 @@ logging.captureWarnings(True)
 class CallbackModule(object):
 
     def __init__(self):
-        #if foo:
-        #    self.disabled = True
+        
         self.insights_token = os.getenv('NR_INSIGHTS_TOKEN')
         self.post_uri = os.getenv('NR_INSIGHTS_URI')
         self.user = getpass.getuser()
